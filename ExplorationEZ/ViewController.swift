57c68f97db638b8f2d29bd4f132419ecd5f9fe4a
Apple <apple@Apples-MacBook-Pro-59.local>
2019-07-31 16:38:28 -0400

    Complete Day 1

--- ExplorationEZ/ViewController.swift
+++ ExplorationEZ/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getStarted(_ sender: Any) {
+        performSegue(withIdentifier: "fromHomeToOverview", sender: self)
+    }
+    
 }
 
